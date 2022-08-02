2271c36a79dc52eaf07c2b1f13bd991486b25318
usman <93078918+khan1756@users.noreply.github.com>
2022-08-03 00:46:02 +0500

    no message

---
+++ school/app/Http/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function data()
+    {
+        return view('admin_user/table');
+    }
+}
